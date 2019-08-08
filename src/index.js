65b0e73af2567a6af074089e959947e04ab29d79
AsheToll <github email address>
2019-08-08 14:54:01 -0400

    Likes updates, but not onclick. Need to fix that and refactor. Done otherwise.

--- src/index.js
+++ src/index.js
@@ -5,12 +5,22 @@ document.addEventListener("DOMContentLoaded", function() {
   fetchAllToys()
 })
 
-let addToyForm = document.querySelector('.add-toy-form')
-addToyForm.addEventListener("submit", newToy)
-
 const toyForm = document.querySelector('.container')
 const addBtn = document.querySelector('#new-toy-btn')
 let addToy = false
+
+addBtn.addEventListener('click', () => {
+  // hide & seek with the form
+  addToy = !addToy
+  if (addToy) {
+    toyForm.style.display = 'block'
+  } else {
+    toyForm.style.display = 'none'
+  }
+})
+
+let addToyForm = document.querySelector('.add-toy-form')
+addToyForm.addEventListener("submit", newToy)
 
 // 1. When DOM loads, GET fetch, create toy divs
 function fetchAllToys() {
@@ -26,6 +36,7 @@ function renderToys(toy) { // pass this in
   let toyDiv = document.createElement('div') // create div
   toyDiv.classList.add('card') // give class of 'card'
   document.getElementById('toy-collection').appendChild(toyDiv) // append to toy collection div
+  toyDiv.dataset.toyId = `${toy.id}`
   
   // Add Toy Info to the Card
   let toyHeader = document.createElement('h2') // h2
@@ -48,6 +59,7 @@ function renderToys(toy) { // pass this in
   toyButton.classList.add('like-btn')
   toyButton.innerText = "Like"
   toyDiv.appendChild(toyButton)
+  toyButton.addEventListener("click", addLikes)
 }
 
 // 2. When toyForm submitted, POST fetch, append toy div container
@@ -66,7 +78,7 @@ function newToy(event) {
     headers:
     {
       "Content-Type": "application/json",
-      Accept: "application/json"
+      "Accept": "application/json"
     },
     body: JSON.stringify(data)
   })
@@ -75,16 +87,22 @@ function newToy(event) {
 }
 
 // 3. When likeButton clicked, PATCH fetch, update likes
-
-addBtn.addEventListener('click', () => {
-  // hide & seek with the form
-  addToy = !addToy
-  if (addToy) {
-    toyForm.style.display = 'block'
-  } else {
-    toyForm.style.display = 'none'
-  }
-})
+function addLikes(event) {
+  let id = event.target.parentElement.dataset.toyId
+  let toy = document.querySelector(`div[data-toy-id = '${id}']`)
+  fetch(`http://localhost:3000/toys/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      "likes": parseInt(toy.children[2].innerText.split(' ')[0]) + 1
+    })
+  })
+  .then(response => response.json())
+  .then(renderToys)
+}
 
 // Things I Need to Review for Better Understanding:
   // creating user stories
@@ -92,3 +110,4 @@ addBtn.addEventListener('click', () => {
   // how to test for each case, & when (besides console.log)
   // tried to renderToys without forEach
   // forgot how to target event?
+  // adding likes was a wild ride
