9921f37c181443ce24b91542f7c15dbbfa6bf99f
Harum Helmy <harumendhah@gmail.com>
2019-08-08 14:52:09 -0400

    got add new toy form working

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-/* User stories!
-1. When the page loads, a user should be able to see Andy's toys
+/* User stories! 
+1. When the page loads, a user should GET and see Andy's toys
+2. A user should be able to POST a new toy to the collection! 
 
 */
-
 document.addEventListener("DOMContentLoaded", ()=>{
   console.log("hello, is it me you're looking for?")
-  addBtn = document.querySelector('#new-toy-btn')
-  toyForm = document.querySelector('.container')
+  
+  // checking for form's existence below: 
+  /* console.log("this is the form element :", 
+    document.querySelector("form")) */
+
+  document.querySelector("form").addEventListener("submit", submitHandler)
+ 
   fetchAllToys()
 })
 
-/* fetches Andy's toys from the JSON server, a GET request  
-invokes callback function to manipulate the response array */
+const addBtn = document.querySelector('#new-toy-btn')
+const toyForm = document.querySelector('.container')
+
+// fetch Andy's toys from the JSON server, a GET request  
+// invokes callback function that renders the response array to the DOM
 function fetchAllToys() {
   fetch('http://localhost:3000/toys')
   .then(res => res.json())
@@ -19,7 +27,8 @@ function fetchAllToys() {
     toyArray.forEach(renderToys) 
   })
 }
-  
+
+//renders toy data to the DOM
 function renderToys(toy) {
     // renders card
     let toyCard = document.createElement("div")
@@ -40,6 +49,7 @@ function renderToys(toy) {
     // renders pTag
     let pTag = document.createElement("p")
     toyCard.appendChild(pTag)
+    pTag.innerText = `${toy.likes} likes`
 
     // renders button 
     let likeButton = document.createElement("button")
@@ -47,6 +57,35 @@ function renderToys(toy) {
     likeButton.innerText = "Like <3"
     toyCard.appendChild(likeButton)
 }
+
+// callback function when user submits new toy form
+function submitHandler(event) {
+  event.preventDefault()
+
+  let newToyInfo = {
+    name: event.target[0].value,
+    image: event.target[1].value,
+    likes: 0
+  }
+
+  let configObject = {
+    method: "POST",
+    headers: {'Content-Type': 'application/json',
+    Accept: 'application/json'
+    },
+    body: JSON.stringify(newToyInfo)
+  } 
+
+  // fetch, but make it POST, you know?
+  fetch('http://localhost:3000/toys', configObject)
+  .then(res => res.json())
+  .then(renderToys)  
+
+  // clears form input box
+  event.target[0].value = ''
+  event.target[1].value = ''
+}
+
 
 let addToy = false
 
@@ -59,6 +98,3 @@ addBtn.addEventListener('click', () => {
     toyForm.style.display = 'none'
   }
 })
-
-
-// OR HERE!
