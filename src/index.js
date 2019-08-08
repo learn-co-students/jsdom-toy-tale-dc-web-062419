9aa30585bd1c128c4ff921ad6fb74d3e4ea2a9e4
Harum Helmy <harumendhah@gmail.com>
2019-08-08 12:04:28 -0400

    got some initial fetch stuff working

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
-const addBtn = document.querySelector('#new-toy-btn')
-const toyForm = document.querySelector('.container')
-let addToy = false
+/* User stories!
+1. When the page loads, a user should be able to see Andy's toys
 
-// YOUR CODE HERE
+*/
+
+document.addEventListener("DOMContentLoaded", ()=>{
+  console.log("hello, is it me you're looking for?")
+  addBtn = document.querySelector('#new-toy-btn')
+  toyForm = document.querySelector('.container')
+  fetchToys()
+})
+
+/* fetches Andy's toys from the JSON server, a GET request  
+invokes callback function to manipulate the response */
+function fetchToys() {
+  fetch('http://localhost:3000/toys')
+  .then(res => res.json())
+  .then(renderToys) 
+}
+
+/* the callback function to manipulate fetch response */
+function renderToys(toyArray) {
+  toyArray.forEach(toy => {
+    console.log(`${toy.name}`)
+  })
+}
+
+let addToy = false
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
