aa66a350ad7eef6b702b40eb9a5a3f203ca89bf1
AsheToll <github email address>
2019-08-08 11:56:31 -0400

    Toy Tale is rendering (empty) toy cards on the page.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
-const addBtn = document.querySelector('#new-toy-btn')
+
+document.addEventListener("DOMContentLoaded", function() {
+  console.log('Page successfully loaded!')
+  fetchAllToys()
+})
+
 const toyForm = document.querySelector('.container')
+const addBtn = document.querySelector('#new-toy-btn')
 let addToy = false
 
-// YOUR CODE HERE
+// 1. When DOM loads, GET fetch, create toy divs
+function fetchAllToys() {
+  fetch('http://localhost:3000/toys')
+  .then(response => response.json()) // turn into JSON
+  .then(toys => {
+    toys.forEach(renderToys)
+  }) // do something with array
+}
+
+// Do something with array from fetch:
+function renderToys(toys) { // pass this in
+  let toyDiv = document.createElement('div') // create div
+  toyDiv.classList.add('card') // give class of 'card'
+  document.getElementById('toy-collection').appendChild(toyDiv) // append to toy collection div
+  
+  // Add Toy Info to the Card
+  let toyHeader = document.createElement('h2') // h2
+}
+
+// 2. When toyForm submitted, POST fetch, append toy div container
+// 3. When likeButton clicked, PATCH fetch, update likes
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
@@ -14,5 +40,8 @@ addBtn.addEventListener('click', () => {
   }
 })
 
-
-// OR HERE!
+// Things I Need to Review for Better Understanding:
+  // creating user stories
+  // review fetch URL
+  // how to test for each case, & when (besides console.log)
+  // tried to renderToys without forEach
